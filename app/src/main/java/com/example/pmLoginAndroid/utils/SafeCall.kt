66bcfc8e2b029668ef6bb11f4bf266f659d91292
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-24 20:07:14 +0200

    Added safeCall

--- app/src/main/java/com/example/pmLoginAndroid/utils/SafeCall.kt
+++ app/src/main/java/com/example/pmLoginAndroid/utils/SafeCall.kt
@@ -1,2 +1,24 @@
 package com.example.pmLoginAndroid.utils
 
+import retrofit2.HttpException
+import java.io.IOException
+
+internal suspend fun <T> safeApiCall(
+    apiCall: suspend () -> T
+): ResultWrapper<T> {
+    return try {
+        ResultWrapper.Success(apiCall.invoke())
+    } catch (throwable: Throwable) {
+        when (throwable) {
+            is IOException -> ResultWrapper.NetworkError
+            is HttpException -> {
+                val code = throwable.code()
+                val errorResponse = throwable.response().toString()
+                ResultWrapper.GenericError(code, errorResponse)
+            }
+            else -> {
+                ResultWrapper.GenericError(null, null)
+            }
+        }
+    }
+}
