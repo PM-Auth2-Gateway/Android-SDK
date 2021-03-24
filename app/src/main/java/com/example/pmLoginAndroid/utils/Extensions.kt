0d42552780e4e890e2f8086a8eeee57add6db827
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-24 20:06:04 +0200

    Added ResultWrapper:

--- app/src/main/java/com/example/pmLoginAndroid/utils/Extensions.kt
+++ app/src/main/java/com/example/pmLoginAndroid/utils/Extensions.kt
@@ -5,10 +5,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-fun View.setVisible(vsbl: Boolean) {
+internal fun View.setVisible(vsbl: Boolean) {
     visibility = if (vsbl) View.VISIBLE else View.GONE
 }
 
-inline fun <reified T : ViewModel> Fragment.injectViewModel(factory: ViewModelProvider.Factory): T {
+internal inline fun <reified T : ViewModel> Fragment.injectViewModel(factory: ViewModelProvider.Factory): T {
     return ViewModelProvider(this, factory)[T::class.java]
 }
