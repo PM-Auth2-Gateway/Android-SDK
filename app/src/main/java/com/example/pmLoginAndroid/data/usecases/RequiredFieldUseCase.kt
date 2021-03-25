c65cf44699c97e41d1c2ca5464c6b0355f8ad3e2
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-26 00:07:11 +0200

    Moved to data package

--- app/src/main/java/com/example/pmLoginAndroid/data/usecases/RequiredFieldUseCase.kt
+++ app/src/main/java/com/example/pmLoginAndroid/data/usecases/RequiredFieldUseCase.kt
@@ -1,4 +1,4 @@
-package com.example.pmLoginAndroid.usecases
+package com.example.pmLoginAndroid.data.usecases
 
 import com.example.pmLoginAndroid.client.PmLogin
 import com.example.pmLoginAndroid.client.model.LoginResult
@@ -8,7 +8,7 @@ import com.example.pmLoginAndroid.data.mapper.ProfileToHashMapper
 class RequiredFieldUseCase(private val options: PmLogin.PmOptions) {
     operator fun invoke(profile: HashMap<String, Any>): LoginResult {
         options.requiredFields.forEach {
-            if(profile[it] == null) return LoginResult.Error(LoginError.NoRequiredFieldsError)
+            if (profile[it] == null) return LoginResult.Error(LoginError.NoRequiredFieldsError)
         }
         return LoginResult.Success(ProfileToHashMapper(options).map(profile))
     }
