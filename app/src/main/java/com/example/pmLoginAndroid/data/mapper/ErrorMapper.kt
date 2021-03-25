c2b15967aab232fdc9a164edd0321cbc002a7d04
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-26 00:08:44 +0200

    Added error mapper

--- app/src/main/java/com/example/pmLoginAndroid/data/mapper/ErrorMapper.kt
+++ app/src/main/java/com/example/pmLoginAndroid/data/mapper/ErrorMapper.kt
@@ -1,4 +1,20 @@
 package com.example.pmLoginAndroid.data.mapper
 
-class ErrorMapper {
+import com.example.pmLoginAndroid.data.LoginError
+import com.example.pmLoginAndroid.utils.ResultWrapper
+import javax.inject.Inject
+
+internal class ErrorMapper @Inject constructor() {
+
+    fun map(error: ResultWrapper.Error): LoginError {
+
+        if (error.isNetworkError) return LoginError.NetworkError
+
+        // TODO return error based on code/message after final contracts are available
+        return when (error.code) {
+            null -> LoginError.GenericError
+            else -> LoginError.GenericError
+        }
+
+    }
 }
