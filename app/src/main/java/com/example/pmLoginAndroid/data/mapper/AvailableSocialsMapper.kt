70c239eae393dae840ec9593d1b9c3e1dc77fd9c
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-26 00:32:27 +0200

    Didn't know you could do that

--- app/src/main/java/com/example/pmLoginAndroid/data/mapper/AvailableSocialsMapper.kt
+++ app/src/main/java/com/example/pmLoginAndroid/data/mapper/AvailableSocialsMapper.kt
@@ -6,16 +6,13 @@ import javax.inject.Inject
 
 internal class AvailableSocialsMapper @Inject constructor() {
 
-    private val availableSocials = listOf(
-        LoginSocial.GOOGLE
-    )
-
     fun map(response: AvailableSocials): List<LoginSocial> {
+
         return response.socials.filter { social ->
-            availableSocials.any { it.id == social.id }
+            LoginSocial.values().any { it.id == social.id }
         }
             .map { social ->
-                availableSocials.find { it.id == social.id }!!
+                LoginSocial.values().find { it.id == social.id }!!
             }
     }
 }
