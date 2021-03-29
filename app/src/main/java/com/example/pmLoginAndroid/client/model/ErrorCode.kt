7faf372a5a0b3372a16e181863445383e6cff69f
NazarDunets <jcq.nazardunets@gmail.com>
2021-03-29 14:59:58 +0300

    Extracted error codes to separate object

---
+++ app/src/main/java/com/example/pmLoginAndroid/client/model/ErrorCode.kt
@@ -0,0 +1,10 @@
+package com.example.pmLoginAndroid.client.model
+
+object ErrorCode {
+    const val SESSION_EXPIRED = 10
+    const val ABORTED = 12
+    const val SERVER_ERROR = 14
+    const val INVALID_ID = 16
+    const val TOKEN_ERROR = 18
+    const val UNAUTHORIZED_ACCESS = 20
+}
--- app/src/main/java/com/example/pmLoginAndroid/client/model/LoginError.kt
+++ app/src/main/java/com/example/pmLoginAndroid/client/model/LoginError.kt
@@ -7,10 +7,10 @@ enum class LoginError(@IdRes val msgId: Int, val errorCode: Int?) {
     NetworkError(R.string.network_error_msg, null),
     GenericError(R.string.generic_error_msg, null),
     NoRequiredFieldsError(R.string.try_another_social_msg, null),
-    UserAbortedLogin(R.string.aborted_authorization_error_msg, 12),
-    SessionIdExpired(R.string.try_again_later_msg, 10),
-    ErrorDuringAuthorization(R.string.try_again_later_msg, 14),
-    InvalidId(R.string.generic_error_msg, 16),
-    TokenError(R.string.generic_error_msg, 18),
-    UnauthorizedAccess(R.string.generic_error_msg, 20)
+    UserAbortedLogin(R.string.aborted_authorization_error_msg, ErrorCode.ABORTED),
+    SessionIdExpired(R.string.try_again_later_msg, ErrorCode.SESSION_EXPIRED),
+    ErrorDuringAuthorization(R.string.try_again_later_msg, ErrorCode.SERVER_ERROR),
+    InvalidId(R.string.generic_error_msg, ErrorCode.INVALID_ID),
+    TokenError(R.string.generic_error_msg, ErrorCode.TOKEN_ERROR),
+    UnauthorizedAccess(R.string.generic_error_msg, ErrorCode.UNAUTHORIZED_ACCESS)
 }
